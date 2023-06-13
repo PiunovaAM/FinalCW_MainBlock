8782654decb179ec6d4d0c96c7110e7c0402d348
Настя <Piu@192.168.1.2>
2023-06-13 16:13:28 +0300

    Add final control homework

--- Final_control_work/Program.cs
+++ Final_control_work/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+//Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+//При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+
+
+string[] CreateUsersTextArray(int size)
+{
+    string[] usersArray = new string[size];
+
+    for (int i = 0; i < usersArray.Length; i++)
+    {
+        Console.Write($"Введите строку {i + 1}: ");
+        usersArray[i] = Console.ReadLine()!;
+    }
+    return usersArray;
+}
+string[] NewTextArray(string[] newArray)
+{
+    int newSize = 0;
+    string[] newTextArray = new string[newSize];
+    int j = 0;
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        if (newArray[i].Length <= 3)
+        {
+            newSize++;
+            Array.Resize(ref newTextArray, newSize);
+            newTextArray[j] = newArray[i];
+            j++;
+        }
+    }
+    return newTextArray;
+}
+
+string[] usersTextArray = CreateUsersTextArray(4);
+string[] newTextArray = NewTextArray(usersTextArray);
+Console.WriteLine($"[{string.Join(", ", usersTextArray)}] -> [{string.Join(", ", newTextArray)}]");
+
